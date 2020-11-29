e7653be9c68828ef09792739236bd31d3dc2eac0
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-11-29 23:29:45 +0800

    2.5: separate module finished

--- part 2/courseinfo/src/component/Course.js
+++ part 2/courseinfo/src/component/Course.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from "react";
 
-const Course =()=>{
-    
-}
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+      {/* <Total course={course} /> */}
+    </>
+  );
+};
 
-export default Course
+const Header = ({ course }) => {
+  console.log("Header", { course });
+  return <h1>{course.name}</h1>;
+};
+
+// const Total = ({ parts }) => {
+//   console.log("part22222", { parts });
+
+// const exercisesCount = courseArr.parts.map((count) => count.exercises);
+// const reducer = (acc, cur) => acc + cur;
+// const sum = exercisesCount.reduce(reducer)
+
+// var sum = exercisesCount.reduce((acc, cur) => acc + cur, 0);
+
+//   return <p>Number of exercises </p>;
+// };
+
+const Part = ({ parts }) => {
+  console.log("parts111", { parts });
+  const sum = parts.map((x) => x.exercises).reduce((a, b) => a + b, 0);
+
+  return (
+    <>
+      {parts.map((ele) => (
+        <div key={ele.id}>
+          <p>
+            {ele.name} {ele.exercises}
+          </p>
+        </div>
+      ))}
+      {<b>total of {sum} exercises</b>}
+    </>
+  );
+};
+
+const Content = ({ course }) => {
+  console.log("content", course[0].parts[0].name);
+  // const partName = course.parts.map((part) => part.name);
+  return (
+    <>
+      {course.map((el) => (
+        <div key={el.id}>
+          <h1>{el.name}</h1>
+          {/* course.parts的内容传给Part组件，单独拿是拿不到的，遍历过后拿到数据再传给组件 */}
+          <Part parts={el.parts} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Course;
