bcd10cce80a396d03f051d4bf2282581ed5502c6
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-11-29 10:31:52 +0800

    2.2: Course information step7 finished

--- part 2/courseinfo/src/index.js
+++ part 2/courseinfo/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Course =({ course })=>{
+  return(
+    <>
+    <Header course={course}/>
+    <Content course={course}/>
+    <Total course={course}/>
+    </>
+  )
+    
+}
+
 const Header = ({ course }) => {
   return (
     <h1>{course.name}</h1>
@@ -33,31 +44,28 @@ const Content = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
+const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
-
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return (<Course course={course} />)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
