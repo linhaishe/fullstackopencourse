ad7a500d3f44c89bde085bb8a3e262b2f6a974f3
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-11-30 16:17:42 +0800

    2.7: The Phonebook step2 finsihed

--- part 2/phonebook/src/index.js
+++ part 2/phonebook/src/index.js
@@ -17,17 +17,41 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
-  const addName = (event) => {    
+  const addName = (event) => {
+    const personsCopy = {...persons}
+    console.log('personsCopy',{personsCopy})
+    console.log('newName',newName)
+    console.log('personsCopy0.name',personsCopy[0].name)
+
+    console.log('addnameconsole',{persons})
+    // alert('提交的名字: ' + newName);    
     event.preventDefault()    
     console.log('button clicked', event.target) 
     //设定一个对象格式，确定数据的保存格式为一个对象
     const nameObject = {
       name: newName
     }
-    	//添加新便笺到便笺列表中，concat方式数组添加
-      setPersons(persons.concat(nameObject))
-      //重置受控input元素的值
-      setNewName('')
+    console.log('addnameObject',nameObject)
+    console.log('nameObject.name',nameObject.name)
+
+    if(String(nameObject.name).replace(/(^\s*)|(\s*$)/g, "").length===0){
+      console.log('nameObject.name',nameObject.name)
+      alert('empty string,plz reconfirm')
+    }else if(JSON.stringify(personsCopy).indexOf(JSON.stringify(nameObject))===-1){
+         //添加新便笺到便笺列表中，concat方式数组添加
+         setPersons(persons.concat(nameObject))
+         //重置受控input元素的值
+         setNewName('')
+    }else{
+      alert(`${newName} is already added to phonebook`)
+    }
+    
+    //question : 去除前后无效空字符串，否则也会被添加
+
+    	// //添加新便笺到便笺列表中，concat方式数组添加
+      // setPersons(persons.concat(nameObject))
+      // //重置受控input元素的值
+      // setNewName('')
   }
 
   const handleNameChange = (event) => {    
@@ -35,9 +59,9 @@ const App = () => {
     setNewName(event.target.value)  
   }
 
-  const myFunction = function myFunction(){
-    alert("你好，我是一个警告框！");
-  }
+  // const myFunction = function myFunction(){
+  //   alert("你好，我是一个警告框！");
+  // }
 
   // const alert=()=>{
   //   alert("你好，我是一个警告框！");
@@ -51,7 +75,7 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit" onClick={myFunction}>add</button>
+          <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
