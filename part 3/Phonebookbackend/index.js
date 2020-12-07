8c29877ec2b6b85576f23d37b31ef54536a47638
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-12-07 17:30:57 +0800

    3.2: Phonebook backend step 2 finished

--- part 3/Phonebookbackend/index.js
+++ part 3/Phonebookbackend/index.js
@@ -1,5 +1,7 @@
 //Node 的内置web server模块,Node 还不支持 ES6模块，es6 ver :import http from 'http'
-const http = require('http')
+// const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = 
     [
@@ -25,13 +27,34 @@ let persons =
       }
     ]
 
+//*Content-Type* 头中的 *application/json* 值通知接收方数据为 JSON 格式。 使用 *JSON.stringify(notes)* 方法将 *notes* 数组转换为 JSON
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(persons))
-})
+
+//const http = require('http')的响应方式
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'application/json' })
+//   response.end(JSON.stringify(persons))
+// })
+
+const infoContent=`phonebook has info for '${persons.length}' people`
+const time = new Date();
+
+//open http://localhost:3001
+app.get('/info', (req, res) => {
+    res.send(infoContent+'<br>'+'<br>'+time)
+  })
+
+//open http://localhost:3001/api/persons
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
 
 //将绑定的HTTP 服务器分配给 app 变量 ，并监听发送到端口3001的 HTTP 请求
+// const PORT = 3001
+// app.listen(PORT)
+// console.log(`Server running on port ${PORT}`)
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
