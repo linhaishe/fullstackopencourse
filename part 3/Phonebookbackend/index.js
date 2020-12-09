350325dd1a43e9ab177669703e4dbeb5fd3f5b19
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-12-09 10:43:48 +0800

    create gitignore & procfile

--- part 3/Phonebookbackend/index.js
+++ part 3/Phonebookbackend/index.js
@@ -2,7 +2,11 @@
 // const http = require('http')
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
+
 const app = express();
+
+app.use(cors())
 
 morgan.token("POST", (req, res) => JSON.stringify(req.body));
 
@@ -119,7 +123,12 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+// const PORT = 3001;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
