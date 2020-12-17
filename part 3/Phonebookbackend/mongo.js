5112b3dbf903bb945b306ba14e83e3803bdb9c26
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-12-17 11:42:38 +0800

    create person schema

--- part 3/Phonebookbackend/mongo.js
+++ part 3/Phonebookbackend/mongo.js
@@ -12,21 +12,27 @@ const url =
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+// persons.save().then(result => {
+//   console.log('persons saved!')
+//   mongoose.connection.close()
+// })
+
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//     console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
