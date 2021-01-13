c15eb84a04cee3803263a8a1a67d3ed1ec4258d2
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-01-13 11:24:01 +0800

    4.4: helper functions and unit tests finished

--- part 4/bloglist/utils/list_helper.js
+++ part 4/bloglist/utils/list_helper.js
@@ -3,7 +3,18 @@ const dummy = (blogs) => {
         return 1
     }
 }
+
+const totalLikes = (blogs)=>{
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+    const likesNum = blogs.map((item)=>item.likes)
+
+    return likesNum.reduce(reducer, 0)
+
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
