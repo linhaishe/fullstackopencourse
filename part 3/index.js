cb0158987adde87048c6be4734faca7dd8fb4015
linhaishe <42030043+linhaishe@users.noreply.github.com>
2023-03-16 00:15:35 +0800

    feat: 3.5-3.6: Phonebook backend step5-6

--- part 3/index.js
+++ part 3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -24,6 +26,11 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
@@ -44,13 +51,44 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((note) => note.id !== id);
 
- console.log(persons);
-  response.status(204).end()
-})
+  console.log(persons);
+  response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  if (persons.find((x) => x.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(persons);
+});
 
 const PORT = 3001;
 app.listen(PORT);
