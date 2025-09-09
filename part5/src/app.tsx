2257a29637bc04cd8ab4bf3f63d428dfa4786f2a
linhaishe <42030043+linhaishe@users.noreply.github.com>
2025-09-09 17:13:31 +0800

    feat: add login form

--- part5/src/app.tsx
+++ part5/src/app.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState, type FormEvent } from 'react';
 import './App.css';
+import Login from './components/Login/Login';
+import loginService from './services/login';
 
 /**
  * ---task1---
@@ -24,30 +24,26 @@ import './App.css';
  */
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [user, setUser] = useState(null);
+
+  function handleLogin(): void {
+    // throw new Error('Function not implemented.');
+    console.log('event', username, password);
+  }
+
+  useEffect(() => {}, []);
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login
+        handleLogin={handleLogin}
+        setUsername={setUsername}
+        setPassword={setPassword}
+        username={username}
+        password={password}
+      />
     </>
   );
 }
