eedb4f529e8566341f39beee0a4a466d7d2c7561
linhaishe <42030043+linhaishe@users.noreply.github.com>
2025-09-16 20:06:56 +0800

    feat: 7.4: Anecdotes and Hooks step 1

--- part7/README.md
+++ part7/README.md
@@ -1,8 +1,15 @@
-# React + Vite
+React offers 15 different built-in hooks, of which the most popular ones are the useState and useEffect hooks that we have already been using extensively.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+React offers the option to create custom hooks. According to React, the primary purpose of custom hooks is to facilitate the reuse of the logic used in components.
 
-Currently, two official plugins are available:
+Building your own Hooks lets you extract component logic into reusable functions.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Custom hooks are regular JavaScript functions that can use any other hooks, as long as they adhere to the rules of hooks. Additionally, the name of custom hooks must start with the word use.
+
+### More about hooks
+
+The internet is starting to fill up with more and more helpful material related to hooks. The following sources are worth checking out:
+
+- [Awesome React Hooks Resources](https://github.com/rehooks/awesome-react-hooks)
+- [Easy to understand React Hook recipes by Gabe Ragland](https://usehooks.com/)
+- [Why Do React Hooks Rely on Call Order?](https://overreacted.io/why-do-hooks-rely-on-call-order/)
--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -9,6 +9,7 @@ import {
   useMatch,
   useNavigate,
 } from 'react-router-dom';
+import { useField } from './hooks';
 import './App.css';
 
 const Menu = () => {
@@ -90,17 +91,17 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
+  const content = useField('');
+  const author = useField('');
+  const info = useField('');
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     navigate('/');
@@ -113,27 +114,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
