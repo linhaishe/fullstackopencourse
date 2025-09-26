280f24cfedd22089777ae6437e0c22bfcfce2af3
linhaishe <42030043+linhaishe@users.noreply.github.com>
2025-09-26 15:21:48 +0800

    feat: 8.14: Database, part 2

--- part8/library-backend-mongodb/graphql.ts
+++ part8/library-backend-mongodb/graphql.ts
@@ -4,7 +4,8 @@ import { v1 as uuid } from 'uuid';
 import Book from './models/books';
 import Author from './models/authors';
 import { GraphQLError } from 'graphql';
-import { authors } from './models/const';
+import { authors } from './const';
+import { TAddAuthorParams, TAddBookParams, TEditAuthorParams } from './types';
 
 export const typeDefs = `
   type Book {
@@ -51,17 +52,40 @@ export const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
-    allBooks: async () => Book.find({}),
+    allBooks: async (root: any, args: { author: string; genre: string }) => {
+      if (args.author && args.genre) {
+        const authorDoc = await Author.findOne({ name: args.author });
+        return Book.find({
+          author: authorDoc?._id,
+          genres: { $in: [args.genre] },
+        });
+      } else if (args.author && !args.genre) {
+        const authorDoc = await Author.findOne({ name: args.author });
+        return Book.find({ author: authorDoc?._id });
+      } else if (args.genre && !args.author) {
+        return Book.find({ genres: { $in: [args.genre] } });
+      } else {
+        return Book.find({});
+      }
+    },
     allAuthors: async () => Author.find({}),
   },
 
   Mutation: {
-    addBook: async (root, args) => {
-      const authorDoc = await Author.findOne({ name: args.author });
-      const book = new Book({ ...args, author: authorDoc._id });
-
+    addBook: async (root: any, args: TAddBookParams) => {
       try {
-        await book.save();
+        const authorDoc = await Author.findOne({ name: args.author });
+        if (!authorDoc) {
+          throw new GraphQLError('Saving AUTHOR failed', {
+            extensions: {
+              code: 'AUTHOR_NOT_FOUND',
+            },
+          });
+        } else {
+          const book = new Book({ ...args, author: authorDoc?._id });
+          await book.save();
+          return book;
+        }
       } catch (error) {
         throw new GraphQLError('Saving book failed', {
           extensions: {
@@ -71,12 +95,13 @@ export const resolvers = {
           },
         });
       }
-      return book;
     },
-    addAuthor: async (root, args) => {
-      const author = new Author({ ...args });
+
+    addAuthor: async (root: any, args: TAddAuthorParams) => {
       try {
+        const author = new Author({ ...args });
         await author.save();
+        return author;
       } catch (error) {
         throw new GraphQLError('Saving book failed', {
           extensions: {
@@ -86,14 +111,24 @@ export const resolvers = {
           },
         });
       }
-      return author;
     },
-    editAuthor: async (root, args) => {
-      const authorDoc = await Author.findOne({ name: args.name });
-      console.log('authorDoc', authorDoc);
-      authorDoc.born = args.setBornTo;
+
+    editAuthor: async (root: any, args: TEditAuthorParams) => {
       try {
-        await authorDoc.save();
+        const authorDoc = await Author.findOne({ name: args.name });
+        if (!authorDoc) {
+          throw new GraphQLError('Editing AUTHOR failed', {
+            extensions: {
+              code: 'AUTHOR_NOT_FOUND',
+            },
+          });
+        } else {
+          authorDoc.born = args.setBornTo;
+          const book = new Book({ ...args, author: authorDoc?._id });
+          await book.save();
+          await authorDoc.save();
+          return authorDoc;
+        }
       } catch (error) {
         throw new GraphQLError('Editing author born failed', {
           extensions: {
@@ -103,8 +138,6 @@ export const resolvers = {
           },
         });
       }
-
-      return authorDoc;
     },
   },
 };
