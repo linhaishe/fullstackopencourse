ba81807a69540901dfb6df50573aed989574fbb9
linhaishe <42030043+linhaishe@users.noreply.github.com>
2025-10-06 12:07:23 +0800

    feat: Exercise 9.16

--- part9/vite-ts-react-app/src/App.tsx
+++ part9/vite-ts-react-app/src/App.tsx
@@ -1,31 +1,42 @@
+import { courseParts, type CoursePart } from './data';
+
 interface IHeaderProps {
   courseName: string;
 }
 
 interface IContentProps {
-  courseParts: { name: string; exerciseCount: number }[];
+  courseParts: CoursePart[];
 }
 
 interface ITotalProps {
   totalExercises: number;
 }
 
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7,
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14,
-    },
-  ];
+  // const courseParts = [
+  //   {
+  //     name: 'Fundamentals',
+  //     exerciseCount: 10,
+  //   },
+  //   {
+  //     name: 'Using props to pass data',
+  //     exerciseCount: 7,
+  //   },
+  //   {
+  //     name: 'Deeper type usage',
+  //     exerciseCount: 14,
+  //   },
+  // ];
 
   const totalExercises = courseParts.reduce(
     (sum, part) => sum + part.exerciseCount,
@@ -40,11 +51,58 @@ const App = () => {
     return (
       <div>
         {props?.courseParts?.map((v, index) => {
-          return (
-            <p>
-              {v.name} {v.exerciseCount}
-            </p>
-          );
+          if (v.kind === 'background') {
+            return (
+              <div>
+                <h3>
+                  {v?.name} {v?.exerciseCount}
+                </h3>
+                <p>{v?.description}</p>
+                <p>{v?.backgroundMaterial}</p>
+                <p></p>
+                <br />
+              </div>
+            );
+          } else if (v.kind === 'group') {
+            return (
+              <div>
+                <h3>
+                  {v?.name} {v?.exerciseCount}
+                </h3>
+                <br />
+              </div>
+            );
+          } else if (v.kind === 'basic') {
+            return (
+              <div>
+                <h3>
+                  {v?.name} {v?.exerciseCount}
+                </h3>
+                <p>{v?.description}</p>
+                <p></p>
+                <br />
+              </div>
+            );
+          } else if (v.kind === 'special') {
+            return (
+              <div>
+                <h3>
+                  {v?.name} {v?.exerciseCount}
+                </h3>
+                <p>{v?.description}</p>
+                <p>
+                  required skills:
+                  {v?.requirements?.map?.((v, i) => (
+                    <span>{v},</span>
+                  ))}
+                </p>
+                <p></p>
+                <br />
+              </div>
+            );
+          } else {
+            return assertNever(v);
+          }
         })}
       </div>
     );
@@ -58,6 +116,7 @@ const App = () => {
     <div>
       <Header courseName={courseName} />
       <Content courseParts={courseParts} />
+      <br />
       <Total totalExercises={totalExercises} />
     </div>
   );
